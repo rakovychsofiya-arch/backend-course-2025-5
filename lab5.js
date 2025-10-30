eb2191fe93631d9dcf1119c5db995c6ecdbfeca3
Sofiya bc2025-5 <rakovychsofiya@gmail.com>
2025-10-31 01:06:03 +0200

    part 2 - get))

--- lab5.js
+++ lab5.js
@@ -1,32 +1,77 @@
-const {program}=require('commander');
+const { program } = require('commander');
 const http = require('node:http');
-const fs=require('fs');
+const fs = require('node:fs').promises;
 const path = require('node:path');
-program 
-.requiredOption('-h,--host <string>','Input IP adress of server')
-.requiredOption('-p,--port <number>','Input Port')
-.requiredOption('-c, --cache <path>', 'Input path ')
-.configureOutput({
-    writeErr: () => {}  
+const fsSync = require('node:fs');
+const superagent = require('superagent');
+program
+  .requiredOption('-h,--host <string>', 'Input IP adress of server')
+  .requiredOption('-p,--port <number>', 'Input Port')
+  .requiredOption('-c, --cache <path>', 'Input path ')
+  .configureOutput({
+    writeErr: () => { }
   });
 program.exitOverride();
 
 try {
   program.parse(process.argv);
 } catch (err) {
-     // Якщо не вказано обов'язкову опцію
-  if (err.code === 'commander.missingMandatoryOptionValue' || 
-      err.message.includes('required option')) {
+  // Якщо не вказано обов'язкову опцію
+  if (err.code === 'commander.missingMandatoryOptionValue' ||
+    err.message.includes('required option')) {
     console.error('Please do required option');
     process.exit(1);
   }
   throw err;
 }
-    const options = program.opts();
-const server = http.createServer(async(req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-  //res.end('Server is running!');
+const options = program.opts();
+
+// --- НАЛАШТУВАННЯ КЕШУ 
+const cachePath = path.resolve(options.cache);
+
+console.log(`Перевірка директорії кешу: ${cachePath}`);
+try {
+  // 1. Перевіряємо, чи папка ВЖЕ ІСНУЄ
+  if (!fsSync.existsSync(cachePath)) {
+    // 2. Якщо ні - створюємо її
+    fsSync.mkdirSync(cachePath, { recursive: true });
+    console.log('Директорію кешу успішно створено.');
+  } else {
+    console.log('Директорія кешу вже існує.');
+  }
+} catch (err) {
+  console.error(`Помилка при створенні директорії кешу: ${err.message}`);
+  process.exit(1); // Не можемо запустити сервер без кешу
+}
+const server = http.createServer(async (req, res) => {
+  console.log(`NEW REQUEST: ${req.method} ${req.url}`);
+  const fileId = req.url.slice(1);
+
+  const filePath = path.join(options.cache, fileId + '.jpeg');
+
+  // --- (GET) ---
+
+  if (req.method === 'GET') {
+    try {
+      const data = await fs.readFile(filePath)
+      res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+      res.end(data); // Відправляємо дані (буфер) картинки
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        // Файл не знайдено - це очікувана помилка
+        res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' });
+        res.end('Not Found');
+      } else {
+        // Інша, неочікувана помилка (напр. EACCES - немає прав)
+        console.error('Помилка сервера при читанні файлу:', err);
+        res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
+        res.end('Internal Server Error');
+      }
+    }
+  }
 });
+
+
 server.listen(options.port, options.host, () => {
   console.log(`Server running on http://${options.host}:${options.port}`);
 });
